10f79b8866ffbd97d0998cb3efe2eed3c843f370
xxx <vaahmerda@gmail.com>
2019-03-12 17:41:25 -0300

    commit

---
+++ src/components/ShortUrl.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="formUrl">
+    <b-form>
+      <label class="sr-only" for="inlineFormInputUrl">Url</label>
+      <b-input
+        class="mb-2 mr-sm-2 mb-sm-0"
+        id="inlineFormInputUrl"
+        placeholder="http://www.google.com"
+      />
+      <br/>
+      <b-button variant="primary">Short It !</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style>
+#formUrl {
+  display: inline-block;
+}
+#inlineFormInputUrl{
+    width: 450px;
+}
+</style>
